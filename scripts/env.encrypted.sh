54cabdbc926ebe7537a6be8fe1a019cbd7f28c63
Jerry Chong <jchong@netbase.com>
2021-04-09 16:49:01 +0800

    Bash script to generate PNGs from Grafana

---
+++ scripts/env.encrypted.sh
@@ -0,0 +1,20 @@
+{
+	"data": "ENC[AES256_GCM,data:oZRtgvjduVNE742UqdOclwMsw8rRoPoFb1h1auS2ZJfteEgPI9cXi6sGTY9w8PftkAvexcdnfmMgqKXhKv97P+kqxzYhkbzrK9usAAzfTP6YdWkCOVacS/1gdwSvlHr6t0UOugS13asv86JYKjIexWeecMcy2peTq2Y=,iv:nodL6oo/DWf7X/QEMDDueOu3j+hTfooWlcFV6D28Ik4=,tag:vbNV/x7/Xe4YEQNqGBPOaQ==,type:str]",
+	"sops": {
+		"kms": null,
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"lastmodified": "2021-04-09T08:47:34Z",
+		"mac": "ENC[AES256_GCM,data:pyfs5cVQOUURnOZcNXKCZxr+m1VNBZVmeYjwNbxwvQ+94oNn6mM574N0olvmjUEzR1BqpGm9niS00e8KkcaZOEkkZYPiymMzMyIuTtuMwvibgJTf966cVx0wt1zdphNMuTA8hL1wrBW5qNIIRpCXaNUvomQM+6agNAj5++aJuuc=,iv:Xx2RsUBsGG9UBRq0bg2FWTVJ9BcdhvXkBHav7Qu1H3c=,tag:YZOGi+9wLBV+jgUl4rN/QQ==,type:str]",
+		"pgp": [
+			{
+				"created_at": "2021-04-09T08:47:34Z",
+				"enc": "-----BEGIN PGP MESSAGE-----\n\nwcBMAyRqUR3GmDR4AQgAinNjkFhN0/tFblxS8uMkxFFxD7WM1Z6Phbor6E/N1qo6\nxcwcpTumCkI+5HrfjnUA5m5Jnx79ya+ob7V2QlhYG4yM/+ssbnjj7fJfMEaOsHbp\nZXrFBygjlPXQr8+QUCWbD3DauIY2Kcg8RMw4eIyY2fJMygCkrUkbaMst4O2QheU1\n84hG1xjwh5xIlrvecQ5yG0xNbCRCkm7Rv1h1oht/EEEXLKU4DGvnNRCRSIZzfCC6\nP1sT8SlhKlZJPM/NPCO5lC/LEjdfPjIkbRDI7CaUU35Wav4f7FsA9IQG/88GDlbn\nwjQsBCK1ttsaQ2i1HN26Ub8mHKPfkTE1Si2zUS7A+NLgAeRYu+b7ZJ82HW2pgJ3V\n/+gj4ahB4CXgAuFByuAm4nk5LgHg3eXFiWh8iixsZnQXV8HB2+qCtYq3KbTZZ1QT\nAJJCNoajKOC75ODuS2B9SXhvuEKsLeJ0r1/iPNAYTOHDewA=\n=1fwu\n-----END PGP MESSAGE-----",
+				"fp": "8FEFAA9142CE1531069FA9C9654899A548A41038"
+			}
+		],
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.6.1"
+	}
+}
---
+++ scripts/grafana.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR=$(dirname "$0")
+source ${DIR}/env.sh
+
+BASE_URL="https://zanglang.grafana.net/render/d-solo/"
+ARGS="orgId=1&width=1000&height=500&tz=UTC"
+
+generate() {
+    if ! timeout 30 curl -H "${AUTH}" -s "${BASE_URL}${1}&${ARGS}" > "${2}"; then
+        rm -f "${2}" &>/dev/null
+    fi
+}
+
+cd "${DIR}"
+#generate "_vb4ur_gz/nodes?panelid=2" "cpu.png"
+#generate "_vb4ur_gz/nodes?panelid=7" "disk.png"
+generate "UJyurCTWz/chain-dashboard?panelId=75" "votingpower.png"
+generate "UJyurCTWz/chain-dashboard?panelId=76" "missedsigns.png"
+generate "UJyurCTWz/chain-dashboard?panelId=77" "signeddelay.png"
+generate "UJyurCTWz/chain-dashboard?panelId=81" "load.png"
+#generate "UJyurCTWz/chain-dashboard?panelid=85" "memory.png"
+
+s3cmd put -M -r *.png "s3://jerrys-pool/"
+
